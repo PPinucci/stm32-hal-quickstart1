1c6da436f04a3dda81ae0cd491b8ab9113d688a5
David O'Connor <the_alchemist@fastmail.com>
2024-01-03 09:41:51 -0500

    Rebuild

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+//! This example shows a complete project, including file structure, and config
+//! needed to flash using an ST-Link. The project structure is based on
+//! [Knurling's app-template](https://github.com/knurling-rs/app-template).
+//! This file demonstrates an overview of this library's features.
+
+//! See the syntax example in the main STM32-HAL repo for a more detailed example.
+
+#![no_main]
+#![no_std]
+
+use cortex_m::{self};
+
+use cortex_m_rt::entry;
+
+// These lines are part of our setup for debug printing.
+use defmt_rtt as _;
+use panic_probe as _;
+
+use defmt::println;
+
+// Import parts of this library we use. You could use this style, or perhaps import
+// less here.
+use stm32_hal2::{self, low_power, pac};
+
+mod init;
+mod setup;
+mod system_status;
+
+pub struct Config {}
+
+// #[rtic::app(device = pac, peripherals = false)]
+// mod app {
+//     use super::*;
+//
+//     #[shared]
+//     pub struct Shared {
+//         pub config: Config,
+//         pub system_status: system_status::SystemStatus,
+//     }
+//
+//     #[local]
+//     pub struct Local {}
+//
+//     #[init]
+//     fn init(cx: init::Context) -> (Shared, Local) {
+//         crate::init::run(cx)
+//     }
+//
+//     #[idle(shared = [], local = [])]
+//     /// In this function, we perform setup code that must occur with interrupts enabled.
+//     fn idle(_cx: idle::Context) -> ! {
+//         loop {
+//             asm::nop();
+//         }
+//     }
+// }
+
+#[entry]
+fn main() -> ! {
+    // This line is required to prevent the debugger from disconnecting on entering WFI.
+    // This appears to be a limitation of many STM32 families. Not required in production code,
+    // and significantly increases power consumption in low-power modes. Not required if not using WFI.
+    stm32_hal2::debug_workaround();
+
+    init::run();
+
+    loop {
+        // low_power::sleep_now();
+        cortex_m::asm::nop();
+    }
+}
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
