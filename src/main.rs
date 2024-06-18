8d557729c8335ecee13f7091a4f6269b9cde4c5d
PPinucci <pietro.pinucci@gmail.com>
2024-06-19 00:46:49 +0200

    Working and flashing with blnky - clocks wrong

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,51 @@
-//! This example shows a complete project, including file structure, and config
-//! needed to flash using an ST-Link. The project structure is based on
-//! [Knurling's app-template](https://github.com/knurling-rs/app-template).
-//! This file demonstrates an overview of this library's features.
-
-//! See the syntax example in the main STM32-HAL repo for a more detailed example.
-
 #![no_main]
 #![no_std]
 
-use cortex_m::{self};
-use cortex_m_rt::entry;
-use defmt::println;
-// These lines are part of our setup for debug printing.
-use defmt_rtt as _;
 use panic_probe as _;
-// Import parts of this library we use. You could use this style, or perhaps import
-// less here.
-use hal::{self, low_power, pac};
+use defmt_rtt as _;
 
-mod init;
-mod setup;
-mod system_status;
-
-pub struct Config {}
-
-// #[rtic::app(device = pac, peripherals = false)]
-// mod app {
-//     use super::*;
-//
-//     #[shared]
-//     pub struct Shared {
-//         pub config: Config,
-//         pub system_status: system_status::SystemStatus,
-//     }
-//
-//     #[local]
-//     pub struct Local {}
-//
-//     #[init]
-//     fn init(cx: init::Context) -> (Shared, Local) {
-//         crate::init::run(cx)
-//     }
-//
-//     #[idle(shared = [], local = [])]
-//     /// In this function, we perform setup code that must occur with interrupts enabled.
-//     fn idle(_cx: idle::Context) -> ! {
-//         loop {
-//             asm::nop();
-//         }
-//     }
-// }
+use cortex_m::{self, delay::{self, Delay}};
+use cortex_m_rt::entry;
+use hal::{
+    clocks::Clocks, dma::{Dma, DmaChannel, DmaInput, DmaPeriph}, gpio::{OutputType, Pin, PinMode, Port}, i2c::I2c, low_power, pac, timer::{Timer, TimerInterrupt}
+};
 
 #[entry]
 fn main() -> ! {
-    // This line is required to prevent the debugger from disconnecting on entering WFI.
-    // This appears to be a limitation of many STM32 families. Not required in production code,
-    // and significantly increases power consumption in low-power modes. Not required if not using WFI.
-    // hal::debug_workaround();
+   let dp = pac::Peripherals::take().unwrap();
+   let cp = pac::CorePeripherals::take().unwrap();
 
-    init::run();
 
-    loop {
-        // low_power::sleep_now();
-        cortex_m::asm::nop();
-    }
-}
+   let clock_cfg = Clocks::default();
+   clock_cfg.setup().unwrap();
 
-// same panicking *behavior* as `panic-probe` but doesn't print a panic message
-// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
-#[defmt::panic_handler]
-fn panic() -> ! {
-    cortex_m::asm::udf()
+   let mut delay = Delay::new(cp.SYST, 90_000_000);
+
+   let mut pe8 = Pin::new(Port::E, 8, PinMode::Output);
+   pe8.set_high();
+
+   let mut timer = Timer::new_tim3(dp.TIM3, 0.2, Default::default(), &clock_cfg);
+   timer.enable_interrupt(TimerInterrupt::Update);
+
+   let mut scl = Pin::new(Port::B, 6, PinMode::Alt(4));
+   scl.output_type(OutputType::OpenDrain);
+
+   let mut sda = Pin::new(Port::B, 7, PinMode::Alt(4));
+   sda.output_type(OutputType::OpenDrain);
+
+//    let mut dma = Dma::new(dp.DMA1);
+//    hal::dma::mux(DmaPeriph::Dma1, DmaChannel::C1, DmaInput::I2c1Tx);
+
+   let mut i2c = I2c::new(dp.I2C1, Default::default(), &clock_cfg);
+
+   loop {
+       i2c.write(0x50, &[1, 2, 3]);
+       pe8.toggle();
+       delay.delay_ms(500);
+
+       // Or:
+    //    i2c.write_dma(0x50, &BUF, DmaChannel::C1, Default::default(), DmaPeriph::Dma1);
+
+    //    low_power::sleep_now();
+   }
 }
